242dee493a465b913eb20b09ef04eca21489bf93
enterpixel <55749032+enterpixel@users.noreply.github.com>
2022-03-02 18:55:00 +0100

    act1, act2
    activity 1 = login/register
    activity 2 = Dashboard

--- app/src/main/java/com/neurinos/MainActivity.java
+++ app/src/main/java/com/neurinos/MainActivity.java
@@ -2,7 +2,11 @@ package com.neurinos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.button);
+        Button signup = (Button) findViewById(R.id.button2);
+
+        login.setOnClickListener(v -> openActivity2()); // Beim Klick auf login
+        signup.setOnClickListener(v -> openActivity2()); // Beim Klick auf registrieren
     }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/neurinos/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.neurinos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.neurinos_menu, menu);
+        return true;
+    }
+}
